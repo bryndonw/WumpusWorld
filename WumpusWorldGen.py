46a36398a3ac6d907facf94a241fd9d362012131
Kieran <ringel.kieran@gmail.com>
2021-10-08 11:01:33 -0600

    Generation

--- WumpusWorldGen.py
+++ WumpusWorldGen.py
@@ -1,9 +1,23 @@
+import random
 
 class WumpusWorldGen():
     def __init__(self):
         pass
 
-    def generateGrid(self, ppit, pwumpus, pobstacle):
-        grid = [][]
-
+    def generateGrid(self, size, ppit, pwumpus, pobstacle):
+        grid = []
+        for i in range(size):
+            row = []
+            for j in range(size):
+                num = random.uniform(0, 1)
+                if num < ppit:
+                    row.append('p')
+                elif ppit < num < ppit + pwumpus:
+                    row.append('w')
+                elif ppit + pwumpus < num < ppit + pwumpus + pobstacle:
+                    row.append('o')
+                else:
+                    row.append('s')
+            grid.append(row)
+        print(grid)
         return grid
--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+from WumpusWorldGen import WumpusWorldGen
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    print('main')
+    puzzle = WumpusWorldGen.generateGrid(WumpusWorldGen, 5, .1, .1, .1)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
