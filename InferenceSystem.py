1ce31a5b198479a1cb228fd702d6bf46f95a187d
Kieran <ringel.kieran@gmail.com>
2021-10-14 12:07:39 -0600

    Last night still doesnt work but yay

--- InferenceSystem.py
+++ InferenceSystem.py
@@ -1,3 +1,5 @@
+#not include &s in KB, every sentence in KB is &ed together
+
 import re
 class InferenceSystem():
     def __init__(self):
@@ -8,12 +10,11 @@ class InferenceSystem():
                      'bump(row,col)': 'o(row,col)',
                      '~bump(row,col)': '~o(row,col)'}
 
-        self.KB = []
-        pass
+        self.KB = ['w(2,3)']
 
     def updateKB(self, rowloc, colloc, percepts):
         z = []
-        self.KB.append('v(' + str(rowloc) + ',' + str(colloc) + ')')
+        #self.KB.append('v(' + str(rowloc) + ',' + str(colloc) + ')')
         if 's' in percepts:
             replace = self.unify('s(row,col)', 's(' + str(rowloc) + ',' + str(colloc) + ')', z)
             newreplace = replace.copy()
@@ -23,7 +24,10 @@ class InferenceSystem():
             check = self.rule['s(row,col)']
             for key, val in newreplace.items().__reversed__():
                 check = check.replace(key, str(val))
-            self.KB.append(check)
+            check = check.split('&')
+            for fact in check:
+                self.KB.append(fact)
+        '''        
         else:
             replace = self.unify('~s(row,col)', '~s(' + str(rowloc) + ',' + str(colloc) + ')', z)
             newreplace = replace.copy()
@@ -64,6 +68,7 @@ class InferenceSystem():
             for key, val in newreplace.items().__reversed__():
                 check = check.replace(key, str(val))
             self.KB.append(check)
+        '''
 
     def unify(self, x, y, z):
        x = re.split(r'[(,)]\s*', x)
@@ -82,13 +87,15 @@ class InferenceSystem():
 
     def bestAction(self, rowloc, colloc):
         actions = [[rowloc + 1, colloc], [rowloc - 1, colloc], [rowloc, colloc + 1], [rowloc, colloc - 1]]
-        for act in actions:
-            #true means ~ ?? I thinkgs
-            wumpus = self.resolution(self.KB, '~w('+ str(act[0]) + ',' + str(act[0]) + ')')
-            pit = self.resolution(self.KB, '~p(' + str(act[0]) + ',' + str(act[0]) + ')')
-            obstacle = self.resolution(self.KB, '~o(' + str(act[0]) + ',' + str(act[0]) + ')')
-            visited = self.resolution(self.KB, '~v(' + str(act[0]) + ',' + str(act[0]) + ')')
-            #do logic on bools to determine best move
+        #for act in actions:
+        #true means ~ ?? I thinkgs
+        act = actions[0]
+        wumpus = self.resolution(self.KB, '~w('+ str(act[0]) + ',' + str(act[1]) + ')')
+        print(wumpus)
+        pit = self.resolution(self.KB, '~p(' + str(act[0]) + ',' + str(act[1]) + ')')
+        obstacle = self.resolution(self.KB, '~o(' + str(act[0]) + ',' + str(act[1]) + ')')
+        visited = self.resolution(self.KB, '~v(' + str(act[0]) + ',' + str(act[1]) + ')')
+        #do logic on bools to determine best move
 
         #check all 4 squares around
         #no wumpus
@@ -96,11 +103,44 @@ class InferenceSystem():
         #no obstacle
         #not visited
 
-        return action
+        #return action
 
     def resolution(self, KB, sentence):
+        currKB = KB.copy()
+        currKB.append(sentence)
+        new = []
+        while True:
+            for KBi in currKB:
+                for KBj in currKB:
+                    if KBi != KBj:
+                        resolvents = self.resolve(KBi, KBj)
+                        if len(resolvents) == 0: return True
+                        new = set(new.append(resolvents))
+            if new.issubset(currKB): return False
+            currKB = set(currKB.append(new))
+
+    def resolve(self, clause1, clause2):
+        clauses = []
+        #drop {}
+        if '|' in clause1:
+            clause1dis = clause1.split('|')
+        else:
+            clause1dis = [clause1]
+        if '|' in clause2:
+            clause2dis = clause2.split('|')
+        else:
+            clause2dis = [clause2]
+        for d1 in clause1dis:
+            for d2 in clause2dis:
+                if d1 == '~' + d2 or '~' + d1 == d2:
+                    list(set(clause2.remove(d1) + clause1.remove(d2)))
+                    clauses.append(list(set(clause2.remove(d1) + clause1.remove(d2))))
+        return clauses
+
+
+
+
 
         # combines rules into FACTS
         #check if lines in KB resolve w/ sentence
         #else add sentence to KB
-        return bool
--- main.py
+++ main.py
@@ -3,13 +3,20 @@ from WumpusWorldGen import WumpusWorldGen
 from Explorer import Explorer
 import random
 
+from InferenceSystem import InferenceSystem
+
 def main():
+    iss = InferenceSystem()
+    iss.updateKB(1, 1, ['s'])
+    iss.bestAction(3,3)
+    '''
     puzzle = WumpusWorldGen.generateGrid(WumpusWorldGen, 5, .1, .1, .1)
     start = WumpusWorldGen.startloc(WumpusWorldGen, puzzle)
     print(start)
     one = Explorer(puzzle)
     test = one.inferenceAgent(start[0], start[1])
     print(test)
+    '''
 
 
 if __name__ == '__main__':
